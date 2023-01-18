364587cb1c5ec5c4c218f2e3e7507146b16bb51c
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-01-19 10:05:40 +1100

    commit tree 1fe5541e5afd6245c230923c4d3e98e9eb01b92c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,24 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!!/).count
   end
 end
+
+
+sentence = "This is a string! It has three sentences. Right?"
+
+pp sentence.split(/\.|\?|!/).count
